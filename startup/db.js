b86149bf5440a6e03a50a371212fd519237a24cb
Sheggsmann <promisesheggs@gmail.com>
2021-05-03 00:51:49 +0100

    Third commit

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,8 @@ const winston = require('winston')
 const config = require('config')
 
 mongoose.Promise = global.Promise
-let mongo_uri = config.get('db')
+let mongo_uri = process.env.vidly_db
+console.log(mongo_uri)
 
 module.exports = function() { 
     // console.log("[DATABASE]:", database)
